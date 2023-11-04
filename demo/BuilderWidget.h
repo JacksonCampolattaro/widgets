64c414268b3b64063bc3ce833460a4895ad63e0a
JacksonCampolattaro <jackcamp@vt.edu>
2023-11-04 13:41:26 +0100

    Experiment with template widgets in Gtkmm

--- demo/BuilderWidget.h
+++ demo/BuilderWidget.h
@@ -2,8 +2,45 @@
 #define WIDGETS_BUILDERWIDGET_H
 
 #include <algorithm>
+#include <iostream>
 
 #include <gtkmm/widget.h>
+#include <gtkmm/builder.h>
+
+#include <glibmm/extraclassinit.h>
+#include <gtk/gtk.h>
+
+class TemplateWidget : public Glib::ExtraClassInit {
+public:
+
+    TemplateWidget(const Glib::ustring &resource) :
+            Glib::ExtraClassInit(
+                    class_init_function,
+                    reinterpret_cast<void *>(&_resource),
+                    instance_init_function
+            ),
+            _resource(resource) {}
+
+private:
+
+    static void class_init_function(void *g_class, void *class_data) {
+        g_return_if_fail(GTK_IS_WIDGET_CLASS(g_class));
+
+        const auto klass = static_cast<GtkWidgetClass *>(g_class);
+        const auto resource = static_cast<Glib::ustring *>(class_data);
+        gtk_widget_class_set_template_from_resource(
+                klass,
+                resource->c_str()
+        );
+    }
+
+    static void instance_init_function(GTypeInstance *instance, void * /* g_class */) {
+        g_return_if_fail(GTK_IS_WIDGET(instance));
+        gtk_widget_init_template(GTK_WIDGET(instance));
+    }
+
+    Glib::ustring _resource;
+};
 
 template<std::size_t N>
 struct ResourcePath {
@@ -11,11 +48,21 @@ struct ResourcePath {
         std::copy_n(str, N, value);
     }
 
+    [[nodiscard]] Glib::ustring ustring() const {
+        return {value};
+    }
+
     char value[N];
 };
 
-template<ResourcePath Path>
-class BuilderWidget : public Gtk::Widget {
+template<ResourcePath Path, class W>
+class BuilderWidget : public TemplateWidget, public W {
+public:
+
+    BuilderWidget() :
+            TemplateWidget(Path.ustring()),
+            Glib::ObjectBase("BuilderWidget"),
+            W() {}
 
 };
 
--- demo/main.cpp
+++ demo/main.cpp
@@ -1,8 +1,55 @@
-#include <gtkmm/builder.h>
 #include <gtkmm/application.h>
+#include <gtkmm/applicationwindow.h>
+
+#include "BuilderWidget.h"
+
+//using DemoWindow = BuilderWidget<
+//        "/campolattaro/jackson/widgets/demo.ui",
+//        Gtk::ApplicationWindow
+//>;
+
+class DemoWindow : public TemplateWidget, public Gtk::ApplicationWindow {
+public:
+
+    DemoWindow() : Glib::ObjectBase("DemoWindow"),
+                   TemplateWidget("/campolattaro/jackson/widgets/demo.ui"),
+                   Gtk::ApplicationWindow() {}
+
+};
+
+
+class DemoApplication : public Gtk::Application {
+protected:
+
+    DemoApplication() : Gtk::Application("campolattaro.jackson.widgets.demo") {}
+
+public:
+
+    static Glib::RefPtr<DemoApplication> create() {
+        return Glib::make_refptr_for_instance<DemoApplication>(new DemoApplication());
+    }
+
+protected:
+
+    // Override default signal handlers:
+    void on_activate() override {
+        auto window = Gtk::make_managed<DemoWindow>();
+        add_window(*window);
+        window->present();
+    }
+
+    void on_open(const Gio::Application::type_vec_files &files,
+                 const Glib::ustring &hint) override {
+
+    }
+
+private:
+
+    void on_hide_window(Gtk::Window *window);
+};
 
 int main() {
-    auto app = Gtk::Application::create("campolattaro.jackson.widgets.demo");
-    auto builder = Gtk::Builder::create_from_resource("/campolattaro/jackson/widgets/demo.ui");
-    // todo
+
+    auto app = DemoApplication::create();
+    return app->run();
 }
